aea285982530322eaa965d98730d446e830289ff
= <=>
2024-10-19 22:56:45 -0300

    delete some config files

--- src/scripts/domObjects.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  coinName: document.querySelector('.coin-name'),
-  coinPrice: document.querySelector('.coin-price'),
-  coinImage: document.querySelector('.coin-image'),
-  coinPriceChange: document.querySelector('.coin-price-change'),
-  lastUpdated: document.querySelector('.last-updated')
-};
