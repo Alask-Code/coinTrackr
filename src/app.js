859779893460a19594840bf7add1cfd3dc90096a
= <=>
2024-10-19 22:22:50 -0300

    added timer

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ async function app () {
   const { fetchCoinPrice, defaultFlags: flags } = require('./scripts/fetchCoinPrice');
   const { updateElements } = require('./scripts/updateElements');
 
-  let btcData = await fetchCoinPrice('bitcoin', 'brl', flags);
+  let btcData = await fetchCoinPrice('solana', 'brl', flags);
   let timestamp = new Date(btcData.last_updated);
   updateElements(btcData, timestamp);
 
   setInterval(async () => {
-    btcData = await fetchCoinPrice('bitcoin', 'brl', flags);
+    btcData = await fetchCoinPrice('solana', 'brl', flags);
     timestamp = new Date(btcData.last_updated);
     updateElements(btcData, timestamp);
-  }, 1000 * 60);
+    console.log(btcData);
+  }, 1000 * 55);
 }
 
 app();
